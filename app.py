ff368e69c3c0bb6fc4401b48113c3acc9589185f
tataa <10979105@gm.nfu.edu.tw>
2020-11-24 16:55:05 +0800

    2

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
+import os
 if __name__ == "__main__":
-    app.run()
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
